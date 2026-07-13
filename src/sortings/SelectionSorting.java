c86f47fb26da9795d57bf6184eb13f82a6477c80
Bhavadeep <manchalabhavadeep18@gmail.com>
2026-07-13 23:47:05 +0530

    add Selection sort code

---
+++ src/sortings/SelectionSorting.java
@@ -0,0 +1,35 @@
+package sortings;
+
+import java.util.Arrays;
+
+public class SelectionSorting {
+    public static void main(String[] args) {
+        int  [] arr = {1,7,3,5,2,9,4};
+        searching(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void searching (int [] arr){
+        for (int i = 0; i < arr.length; i++) {
+            int last = arr.length - i -1;
+            int maxIndex = getMaxIndex(arr , 0 , last );
+            swap(arr, maxIndex, last);
+
+        }
+    }
+    static void swap(int [] arr, int first , int second){
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+
+    static int getMaxIndex(int [] arr, int start, int end){
+        int max = start;
+        for (int i = start; i <=end ; i++) {
+            if (arr[max] < arr[i]) {
+                max = i;
+            }
+
+        }
+        return max;
+    }
+}
