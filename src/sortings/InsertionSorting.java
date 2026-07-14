23d0c737c08b113cff7b2f5249e36a538900c996
Bhavadeep <manchalabhavadeep18@gmail.com>
2026-07-14 23:28:31 +0530

    add comment for details

--- src/sortings/InsertionSorting.java
+++ src/sortings/InsertionSorting.java
@@ -16,7 +16,7 @@ public class InsertionSorting {
 
     }
     static void Sorting(int [] arr){
-        for (int i = 0; i < arr.length - 1; i++) {
+        for (int i = 0; i < arr.length - 1; i++) { // here i is < arr.length -1 unlike other sorting techniques
             for (int j = i+1; j >0 ; j--) {
                 if (arr[j] < arr[j-1]){
                     swap(arr ,j, j-1);
