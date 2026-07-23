b4a081e1008b518b715abf5d0cc5560ee02b2e62
Bhavadeep <manchalabhavadeep18@gmail.com>
2026-07-23 22:20:28 +0530

    add string codes

---
+++ src/strings/Methods.java
@@ -0,0 +1,15 @@
+package strings;
+
+import java.util.Arrays;
+
+public class Methods {
+    public static void main(String[] args) {
+        String name = "Manchala Bhavadeep";
+        System.out.println(Arrays.toString(name.toCharArray()));
+        System.out.println(name.toLowerCase());
+        System.out.println(name.charAt(6));
+        System.out.println(name.length());
+        System.out.println(name.trim());
+
+    }
+}
--- src/strings/Performance.java
+++ src/strings/Performance.java
@@ -4,8 +4,8 @@ public class Performance {
     public static void main(String[] args) {
         String series = "";
         for (int i = 0; i <26 ; i++) {
-            char ch = (char) ('a' + i);
+            char ch = (char) ('a' + i); //series = series + ch; to print in the form of string
             System.out.println(ch);
         }
-    }
+    }// system.out.println(series); after the loop;
 }
