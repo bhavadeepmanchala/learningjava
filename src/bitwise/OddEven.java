0ea2301c45153ede79f810726972cf6c34404032
Bhavadeep <manchalabhavadeep18@gmail.com>
2026-07-29 23:36:51 +0530

    Add Bitwise Operators programs

--- src/bitwise/OddEven.java
+++ src/bitwise/OddEven.java
@@ -1,4 +1,15 @@
 package bitwise;
 
+import java.util.Scanner;
+
 public class OddEven {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int num = in.nextInt();
+        if((num & 1) == 1){
+            System.out.println("odd");
+        }else {
+            System.out.println("even");
+        }
+    }
 }
--- src/bitwise/Unique.java
+++ src/bitwise/Unique.java
@@ -1,4 +1,17 @@
 package bitwise;
 
 public class Unique {
+    public static void main(String[] args) {
+        int [] arr ={1,2,3,2,4,3,1};
+        System.out.println(answer(arr));
+    }
+    static int answer(int [] arr){
+        int unique = 0;
+
+        for(int n : arr) {
+            unique ^= n;
+        }
+        return unique;
+    }
+
 }
