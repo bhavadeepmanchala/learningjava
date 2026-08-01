67e32fb257a48c07e0e082018e716eb1e06cf7a0
Bhavadeep <manchalabhavadeep18@gmail.com>
2026-08-01 23:57:28 +0530

    Add Bitwise Operators program

--- src/bitwise/Digits.java
+++ src/bitwise/Digits.java
@@ -1,4 +1,10 @@
 package bitwise;
 
 public class Digits {
+    public static void main(String[] args) {
+        int n = 5;//gives no of digits in binary form for a given number in decimal form
+        int b = 2;
+        int ans = (int)(Math.log(n)/Math.log(b))+1;
+        System.out.println(ans);
+    }
 }
---
+++ src/bitwise/PowerOfTwo.java
@@ -0,0 +1,9 @@
+package bitwise;
+
+public class PowerOfTwo {
+    public static void main(String[] args) {
+        int n = 16;
+        boolean ans = (n &(n-1)) == 0;
+        System.out.println(ans);
+    }
+}
