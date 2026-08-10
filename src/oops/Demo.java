954b3d3b1fcadfecd22575875d7ac164c67c43b6
Bhavadeep <manchalabhavadeep18@gmail.com>
2026-08-10 23:21:20 +0530

    Add basic OOP program

---
+++ src/oops/Demo.java
@@ -0,0 +1,29 @@
+package oops;
+
+public class Demo {
+    public static void main(String[] args) {
+        Students ob = new Students();
+        System.out.println(ob.name);
+        Students ib = new Students(11,23,34);
+        System.out.println(ib.name);
+        Students er = new Students();
+        System.out.println(er.name);
+    }
+}
+class Students{
+    int name;
+    int add ;
+    int city;
+
+    Students(){
+      name = 103;
+      add =19;
+      city = 56;
+    }
+    Students(int nae , int adds , int dcity){
+      name = nae;
+      add = adds;
+      city = dcity;
+    }
+
+}
